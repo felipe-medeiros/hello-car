ad87474b532be920fd2e1d5ae774019410e9e9aa
Felipe Medeiros <felipe.medeiros.alves@gmail.com>
2020-07-30 09:20:58 -0300

    migrations e controller

---
+++ database/migrations/2020_07_30_115444_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa', 7)->unique();
+            $table->date('data_compra');
+            $table->date('data_venda');
+            $table->enum('situacao', ['Em uso', 'Em manutenção', 'Vendido']);
+            $table->foreign('modelo_id')->references('id')->on('types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
